19b9298a1354c2ef062223c05f51f117d536a04e
Sujith <110763552+userSUJITH@users.noreply.github.com>
2026-08-22 18:22:05 +0530

    Easy: 561. Array Partition | Approach 1 | Runtime: 17 | Memory: 49888000

---
+++ 561. Array Partition/Approach_1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+     int ans =0;
+     Arrays.sort(nums);
+     for(int i=0;i<nums.length;i=i+2){
+        ans += nums[i];
+     }   
+     return ans;
+    }
+}
